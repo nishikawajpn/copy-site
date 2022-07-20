9a5c20a8c7a61dc724f82ea5f77269704cd30b3e
nishikawajpn <99392200+nishikawajpn@users.noreply.github.com>
2022-07-21 06:18:13 +0900

    create decoration

--- src/js/_menu.js
+++ src/js/_menu.js
@@ -4,8 +4,24 @@ export default (() => {
   let globalNav = document.getElementById('global-nav');
 
   globalNavButton.addEventListener('click', e => {
-    e.currentTarget.classList.toggle('is-clicked');
-    globalNav.classList.toggle('is-visible');
+    toggleMenu(e.currentTarget, globalNav);
   });
+
+
+  let globalNavLinks = document.querySelectorAll('.global-nav__link');
+  let globalNavReservationLink = document.querySelector('.global-nav__reservation');
+  globalNavLinks = [...globalNavLinks, globalNavReservationLink];
+
+  globalNavLinks.forEach(item => {
+    item.addEventListener('click', () => {
+      toggleMenu(globalNavButton, globalNav);
+    })
+  })
+
+  
+  function toggleMenu(button, navBody) {
+    button.classList.toggle('is-clicked');
+    navBody.classList.toggle('is-visible');
+  }
 
 })();
